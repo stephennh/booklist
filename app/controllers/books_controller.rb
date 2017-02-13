acde5c5ad12902831da01d5a9e6202c5e8e48bae
Stephen H <stephen210huang@hotmail.com>
2017-02-13 13:50:00 -0500

    created CSV and JSON view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,17 @@
 class BooksController < ApplicationController
 
   def index
+    @books = Book.all
+    respond_to do |format|
+      format.html
+      format.text
+      format.csv do
+        render plain: Book.generate_csv(@books)
+      end
+      format.json do
+        render json: @books
+      end
+    end
   end
 
 end
